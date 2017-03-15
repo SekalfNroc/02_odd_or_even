bc30d1b1ab489d214c3ab9b198c0a42f6ef5ec73
Sekalf Nroc <sekalf.256@gmail.com>
2017-03-15 19:55:27 +0000

    2.0.0 Added Dynamic Checking
    Ask the user for two numbers. If the second divides evenly into the
    first, tell that to the user.

--- __main__.py
+++ __main__.py
@@ -13,8 +13,15 @@ number_type = "even" if duple_remainder == 0 else "odd"
 quadruple_remainder = number % 4
 is_multiple_of_four = quadruple_remainder == 0
 
-if is_multiple_of_four:
-    print("You picked a multiple of four")
-else:
-    print("You picked an %s number." % number_type)
+secondary_divisor = int(input("Enter a number: \n> "))
+secondary_remainder = number % secondary_divisor
+is_multiple_of_divisor = secondary_remainder == 0
 
+if is_multiple_of_divisor:
+    descriptor = "a multiple of the second number."
+elif is_multiple_of_four:
+    descriptor = "a multiple of four."
+else:
+    descriptor = "an %s number." % number_type
+
+print("You picked %s" % descriptor)
