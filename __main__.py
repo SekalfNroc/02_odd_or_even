f8be9c0e2bc5915f498774ecbb04fdb51559d742
Sekalf Nroc <sekalf.256@gmail.com>
2017-03-15 19:44:17 +0000

    1.1.0 Quadruple Checking
    If the number is a multiple of four, print out a different message.

--- __main__.py
+++ __main__.py
@@ -10,3 +10,11 @@ while True: # Broken on success
 duple_remainder = number % 2
 number_type = "even" if duple_remainder == 0 else "odd"
 
+quadruple_remainder = number % 4
+is_multiple_of_four = quadruple_remainder == 0
+
+if is_multiple_of_four:
+    print("You picked a multiple of four")
+else:
+    print("You picked an %s number." % number_type)
+
